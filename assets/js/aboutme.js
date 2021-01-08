89eded9d8eade89ce6a20d4a6169dec7738c5916
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-08 16:51:21 +0900

    me-transiiton으로 글자 보이고 숨기기

--- assets/js/aboutme.js
+++ assets/js/aboutme.js
@@ -2,25 +2,20 @@ $(document).ready(function () {
   var timer = 0;
   var scrollY;
 
-  $('#aboutme .think').stop().animate({'opacity': 1, 'top': 0});
+  $('#aboutme').addClass('load');
 
   $(window).on('scroll', function () {
     clearTimeout(timer);
 
     timer = setTimeout(function () {
       scrollY = $(this).scrollTop();
-      $('#aboutme .my_feature').not('.think').each(function () {
-        var cntY = $(this).offset().top;
-        if (scrollY < cntY) $(this).stop().animate({
-          'opacity': 0,
-          'top': 40
-        });
-        else $(this).stop().animate({
-          'opacity': 1,
-          'top': 0
-        });
+      $('#aboutme .hide').each(function () {
+        var cntY = $(this).offset().top - $(window).height() * 0.8;
+        if (scrollY > cntY) {
+          $(this).addClass('fadein');
+        }
       });
-    }, 200);
+    }, 50);
   });
 
 });
