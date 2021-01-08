5e66caf2788e74e960d719866e3e1de7c2078062
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-08 11:54:42 +0900

    aboutme-fadein적용

--- assets/js/aboutme.js
+++ assets/js/aboutme.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $('#aboutme').addClass('appear');
-  }, 500);
+$(document).ready(function () {
+  var timer = 0;
+  var scrollY;
+
+  $(window).on('scroll', function () {
+    clearTimeout(timer);
+
+    timer = setTimeout(function () {
+      scrollY = $(this).scrollTop();
+      $('#aboutme .my_feature').each(function () {
+        var cntY = $(this).offset().top;
+        if (scrollY < cntY) $(this).stop().animate({
+          'opacity': 0,
+          'top': 40
+        });
+        else $(this).stop().animate({
+          'opacity': 1,
+          'top': 0
+        });
+      });
+    }, 200);
+  });
+
 });
