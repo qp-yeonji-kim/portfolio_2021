d94d6b9fd3092cede8f066fdbe0eab9a61f778ab
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-09 23:58:05 +0900

    me-svg통한 애니메이션 html마크업
    기존에 배운 stroke-dasharray로는 움직이지 않았지만 기록해둔다.

--- assets/js/aboutme.js
+++ assets/js/aboutme.js
@@ -18,4 +18,17 @@ $(document).ready(function () {
     }, 50);
   });
 
+  $('#main-project').on('mousemove', function (e) {
+    if (e.pageX < $(window).width() / 2) {
+      $('#main-project').addClass('left');
+    } else {
+      $('#main-project').removeClass('left');
+    }
+  });
+
+  $('#main-project .link a').on('click', function (e) {
+    e.preventDefault();
+    $(this).parent().addClass('click');
+
+  });
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,5 @@ $(document).ready(function () {
       }
     }, 1000);
   });
+  
 });
