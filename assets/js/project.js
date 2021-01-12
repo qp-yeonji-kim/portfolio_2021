9dbd88cf3ce13c0384bf13cdd2e57811787dc31d
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-12 16:57:18 +0900

    project-cgv-.view enter키 제어

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,33 +1,35 @@
 $(document).ready(function () {
   $('#project-list').addClass('load');
   $('.site-pic, .site-code').attr('tabindex', 0);
-  $('.site-code').on({
-    click: function () {
-      var $viewDiv = $(this);
-      var $siblingDiv = $(this).siblings('.site-code, .site-pic');
-      if (!$(this).is('.view')) {
-        
-        $siblingDiv.removeClass('view').stop().animate({width: 0}, 500, function(){
-          $(this).hide();
-        });
-        setTimeout(function(){
-          $viewDiv.addClass('view');
-        }, 90);
 
-      } else {
-        $(this).removeClass('view');
-        setTimeout(function(){
-          $siblingDiv.show().animate({width: 448}, 700);
-        }, 90);
-      }
-    },
-    hover: function () {
+  $('.site-code').on('click', function () {
+    var $viewDiv = $(this);
+    var $siblingDiv = $(this).siblings('.site-code, .site-pic');
+    if (!$(this).is('.view')) {
+      $siblingDiv.removeClass('view').stop().animate({width: 0}, 500, function(){
+        $(this).hide();
+      });
+      setTimeout(function(){
+        $viewDiv.addClass('view');
+      }, 90);
 
+    } else {
+      $(this).removeClass('view');
+      setTimeout(function(){
+        $siblingDiv.show().animate({width: 448}, 700);
+      }, 90);
     }
   });
-  $('.site-pic').on({
-    click: function () {
+
+  $('.site-code').on('keydown', function(e){
+    if(e.keyCode === 13) $(this).trigger('click');
+  });
+  
+  $('.site-pic').on('click', function () {
       $(this).toggleClass('view');
-    }
   })
+
+  $('.site-pic').on('keydown', function(e){
+    if(e.keyCode === 13) $(this).trigger('click');
+  });
 });
