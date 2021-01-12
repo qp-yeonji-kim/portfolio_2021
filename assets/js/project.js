ac479448470a12780cc8422430ba0de5bdfb4324
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-12 12:32:16 +0900

    project-cgv-.view 제이쿼리
    클릭하면 자연스럽게 이동한다. 기쁘다.

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,13 +1,33 @@
 $(document).ready(function () {
   $('#project-list').addClass('load');
-
   $('.site-pic, .site-code').attr('tabindex', 0);
+  $('.site-code').on({
+    click: function () {
+      if ($(this).not('.view')) {
+        var $clickDiv = $(this);
+        $(this).addClass('view').siblings('.site-code, .site-pic').removeClass('view').stop().animate({width: 0}, 500, function(){
+          $(this).hide();
+        });
+      } else {
+        $(this).removeClass('view').siblings('.site-code, .site-pic').stop().animate({opacity: 1}, 1000, function(){
+          $(this).show();
+          $clickDiv.removeClass('view');
+        });
+      }
+    },
+    hover: function () {
 
-
+    }
+  });
+  $('.site-pic').on({
+    click: function () {
+      alert();
+    }
+  })
   /* 
   -tab-index만 주면 바로 focus동작할 수 있을까?
   -함수 바깥쪽에 만들고
   안쪽에서 함수 소환하는데
-  함수에 지역변수를 미리 써도 동작하는지? 
+  함수에 지역변수를 미리 써도 동작하는지?   
   */
 });
