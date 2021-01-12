f78e927ce0e58c725d77da35c4a94622033df2f0
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-12 13:31:32 +0900

    project-cgv-완전히 통제는 실패

--- assets/js/project.js
+++ assets/js/project.js
@@ -3,16 +3,13 @@ $(document).ready(function () {
   $('.site-pic, .site-code').attr('tabindex', 0);
   $('.site-code').on({
     click: function () {
-      if ($(this).not('.view')) {
-        var $clickDiv = $(this);
+      if (! $(this).is('.view')) {
         $(this).addClass('view').siblings('.site-code, .site-pic').removeClass('view').stop().animate({width: 0}, 500, function(){
           $(this).hide();
+          setTimeout
         });
       } else {
-        $(this).removeClass('view').siblings('.site-code, .site-pic').stop().animate({opacity: 1}, 1000, function(){
-          $(this).show();
-          $clickDiv.removeClass('view');
-        });
+        $(this).removeClass('view').siblings('.site-code, .site-pic').delay(100).show().stop().animate({width: 448}, 800);
       }
     },
     hover: function () {
@@ -25,7 +22,6 @@ $(document).ready(function () {
     }
   })
   /* 
-  -tab-index만 주면 바로 focus동작할 수 있을까?
   -함수 바깥쪽에 만들고
   안쪽에서 함수 소환하는데
   함수에 지역변수를 미리 써도 동작하는지?   
