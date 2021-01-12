6c487c24aa7523454edde46269eedbf6e32dee78
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-12 09:30:56 +0900

    project-cgv-활성화 시 속성설정
    .view

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('#project-list').addClass('load');
+
+  $('.site-pic, .site-code').attr('tabindex', 0);
+
+
+  /* 
+  -tab-index만 주면 바로 focus동작할 수 있을까?
+  -함수 바깥쪽에 만들고
+  안쪽에서 함수 소환하는데
+  함수에 지역변수를 미리 써도 동작하는지? 
+  */
 });
