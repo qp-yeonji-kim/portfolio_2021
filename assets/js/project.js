2410e105f3231915e7a65e8e2b4b7b521c3d27db
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-03-01 14:08:11 +0900

    기기 크기에 따른 활성화
    gist 코드 확대 축소

--- assets/js/project.js
+++ assets/js/project.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
     },
   });
 
+  // 파도 움직임
   var waving = anime({
     targets: '.wave',
     d: [{
@@ -41,33 +42,7 @@ $(document).ready(function () {
     $(this).toggleClass('tip');
   });
 
-  $('.site-code').on('click', function () {
-    var $viewDiv = $(this);
-    var $siblingDiv = $(this).siblings('.site-code, .site-pic');
-    if (!$(this).is('.view')) {
-      $siblingDiv.removeClass('view').stop().animate({
-        width: 0
-      }, 500, function () {
-        $(this).hide();
-      });
-      setTimeout(function () {
-        $viewDiv.addClass('view');
-      }, 90);
-
-    } else {
-      $(this).removeClass('view');
-      setTimeout(function () {
-        $siblingDiv.show().animate({
-          width: 448
-        }, 700);
-      }, 90);
-    }
-  });
-
-  $('.site-code').on('keydown', function (e) {
-    if (e.keyCode === 13) $(this).trigger('click');
-  });
-
+  // 이미지 clip도형 나타내기
   $('.site-pic').on('click', function () {
     $(this).toggleClass('view');
   })
@@ -75,4 +50,34 @@ $(document).ready(function () {
   $('.site-pic').on('keydown', function (e) {
     if (e.keyCode === 13) $(this).trigger('click');
   });
+
+  // 코드부가 설명
+  if (windowSize > 600) {
+    $('.site-code').on('click', function () {
+      var $viewDiv = $(this);
+      var $siblingDiv = $(this).siblings('.site-code, .site-pic');
+      if (!$(this).is('.view')) {
+        $siblingDiv.removeClass('view').stop().animate({
+          width: 0
+        }, 500, function () {
+          $(this).hide();
+        });
+        setTimeout(function () {
+          $viewDiv.addClass('view');
+        }, 90);
+
+      } else {
+        $(this).removeClass('view');
+        setTimeout(function () {
+          $siblingDiv.show().animate({
+            width: 448
+          }, 700);
+        }, 90);
+      }
+    });
+
+    $('.site-code').on('keydown', function (e) {
+      if (e.keyCode === 13) $(this).trigger('click');
+    });
+  }
 });
