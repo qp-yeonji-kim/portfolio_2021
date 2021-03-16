82938b05ced5f98fe6e869700d7c52e6c7d5bf8b
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-03-16 15:27:57 +0900

    메소드 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
   $('#main-cnt .link a').on('click', function (e) {
     e.preventDefault();
     $(this).parent().addClass('bigger').siblings('.mysite').fadeOut();
+    
     setTimeout(function () {
-      if ($(this).parent().is('#intro')) {
+      if ($(this).parent().has('#intro')) {
         location.href = 'introduce/me.html';
       } else {
         location.href = 'project/project.html';
